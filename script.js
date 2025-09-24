33f3dc91dc19e9f692ab8b87306d70a08557d015
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2025-09-24 10:45:29 -0700

    Updated function buttons for each bg

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const locationButtons = document.querySelectorAll('.function-button');
-
 const body = document.body;
+const actionButtons = document.querySelectorAll('#action-btn-section > div');
+
+function updateView(location) {
+    // Remove previous background
+    body.classList.remove('bedroom-background', 'bathroom-background', 'kitchen-background', 'home-background');
+
+    // Add new background
+    body.classList.add(`${location}-background`);
+
+    // Hide action buttons
+    actionButtons.forEach(actionDiv => {
+        actionDiv.style.display = 'none';
+    });
+
+    // Show only button for the image
+    const currentActionDiv = document.querySelector(`[data-action="${location}"]`);
+    if (currentActionDiv) {
+        currentActionDiv.style.display = 'block';
+    }
+}
 
 locationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        
         const location = button.dataset.location;
-        
-        body.classList.remove('bedroom-background', 'bathroom-background', 'kitchen-background', 'home-background');
-        
-        body.classList.add(`${location}-background`);
+        updateView(location);
     });
+});
+
+// Define initial page state (one more)
+document.addEventListener('DOMContentLoaded', () => {
+    updateView('home');
 });
